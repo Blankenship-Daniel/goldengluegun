17bbef119d635a123209e97c978b7b0db54e1e4e
Daniel Blankenship <dblankenship@pro-dblanken1-m.corp.endurance.com>
2017-06-29 15:35:44 -0600

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiEndpoint: 'https://www.googleapis.com/blogger/v3/',
+  apiKey: 'AIzaSyBmBDJ8R1DqDMoD5GNdbgYTbc-qASWxScs',
+  blogId: '7830550230531159274',
+  blogUrl: 'http://thegoldengluegun.blogspot.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,9 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiEndpoint: 'https://www.googleapis.com/blogger/v3/',
+  apiKey: 'AIzaSyBmBDJ8R1DqDMoD5GNdbgYTbc-qASWxScs',
+  blogId: '7830550230531159274',
+  blogUrl: 'http://thegoldengluegun.blogspot.com/'
 };
